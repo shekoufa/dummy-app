79ed4a13747595e12103e88c489e790e004861fb
Navid Shekoufa <shekoufa.navid@gmail.com>
2015-09-15 11:40:36 -0230

    m

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -109,7 +109,7 @@ function sendAjax(type) {
                             var teams = data.data.editable_teams;
                             var divStr = "";
                             for (var i in teams){
-                                divStr = divStr + '<div ontouchend="setTeamId('+teams[i].id+');" id="team-div'+teams[i].id+'" class="team-div" style="border: 2px solid darkgray; display: inline-block; overflow: hidden; padding: 10px; margin: 5px auto; ' +
+                                divStr = divStr + '<div onclick="setTeamId('+teams[i].id+');" id="team-div'+teams[i].id+'" class="team-div" style="border: 2px solid darkgray; display: inline-block; overflow: hidden; padding: 10px; margin: 5px auto; ' +
                                     'width: 40%; margin-right:5px; min-height:120px;' +
                                     'border-radius: 25px;">' +
                                     '<div style="border-radius: 50px; overflow: hidden;margin-bottom: 15px;">';
